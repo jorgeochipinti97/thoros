65423ecf5ad86a2c585bfc3e1337adff243a8784
jorgeochipinti97 <jorgeochipinti97@gmail.com>
2024-03-14 16:46:45 -0300

    2

--- src/components/video.js
+++ src/components/video.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const BackgroundVideo = ({srcFile,  children }) => {
     return (
       <div className='w-screen  flex justify-center relative    ' style={{backgroundColor:'black'}}>
-        <video autoPlay="autoplay" loop="loop" muted className='w-screen rounded-xl shadowbox ' style={{backgroundPosition:'center', opacity:.6}} >
+        <video autoPlay controls loop muted className='w-screen rounded-xl shadowbox ' style={{backgroundPosition:'center', opacity:.6}}  playsInline>
           <source src={srcFile} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
